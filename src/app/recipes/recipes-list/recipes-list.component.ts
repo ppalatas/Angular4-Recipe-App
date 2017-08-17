a4b0f8a98867b224de26acf2406f2c3c4d936e78
Phillip Palatas <Phillip.Palatas@vml.com>
2017-08-17 15:08:38 -0500

    Recipe list and details added

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'This is simply a test', 'http://images.media-allrecipes.com/userphotos/560x315/1073048.jpg'),
+    new Recipe('Test recipe', 'This is simply a test', 'http://images.media-allrecipes.com/userphotos/560x315/1073048.jpg'),
+    new Recipe('Test recipe', 'This is simply a test', 'http://images.media-allrecipes.com/userphotos/560x315/1073048.jpg')
+    // this is where the 3 arguments from recipe.model.ts info comes into play. 
+    // i.e. name, recipe, imagepath
+  ];
   constructor() { }
 
   ngOnInit() {
